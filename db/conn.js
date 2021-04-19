c4c5491f52de36b4e2c2471473b40c4b10ad2280
raghav <raghavjindal131@gmail.com>
2021-04-19 08:43:24 +0530

    changes connection uri

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Typoo", {
+mongoose.connect("mongodb+srv://raghav:Raghav@1@cluster0.5diwb.mongodb.net/typoo?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
